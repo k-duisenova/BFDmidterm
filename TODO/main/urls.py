286b80c550590d2601e00f17311708c498324d26
kuralay <kuralaiduisenova97@gmail.com>
2021-03-20 12:28:10 +0600

    update

--- TODO/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from main.views import todos_list, completed_todos_list
-
-urlpatterns = [
-    path('todos', todos_list),
-    path('todos/1/completed', completed_todos_list)
-]
--- TODO_lab5/auth_/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from auth_.models import User
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('name', 'email')
